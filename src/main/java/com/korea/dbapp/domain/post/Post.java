a16a3b08797194d7493ee566313526247b0c7804
DESKTOP-NUOPUIF\ITPS <heansum1556@gmail.com>
2021-07-02 16:25:24 +0900

    1. ORM 매핑 시작

---
+++ src/main/java/com/korea/dbapp/domain/post/Post.java
@@ -0,0 +1,63 @@
+package com.korea.dbapp.domain.post;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+
+import com.korea.dbapp.domain.user.User;
+
+@Entity
+public class Post {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id; // 프라이머리(기본키)
+	private String title; // varchar(255), 255는 글자수
+	@Lob
+	private String content;
+	
+	 // DB에 맞추기 위해서 객체를 못가져와서 씀
+	
+	// ORM 으로 포린키를 만들어보자
+	@JoinColumn(name = "user_id")
+	@ManyToOne
+	private User user; // ORM 사용
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	
+	
+}
