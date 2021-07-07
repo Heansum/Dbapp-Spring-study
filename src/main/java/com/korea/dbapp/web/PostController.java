e83eaf47f16233c8262634e9fc30b646768ef6c2
DESKTOP-NUOPUIF\ITPS <heansum1556@gmail.com>
2021-07-07 17:46:37 +0900

    6. 메인페이지, 글쓰기 목록 구현

--- src/main/java/com/korea/dbapp/web/PostController.java
+++ src/main/java/com/korea/dbapp/web/PostController.java
@@ -1,13 +1,28 @@
 package com.korea.dbapp.web;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.korea.dbapp.domain.post.PostRepository;
 
 @Controller
 public class PostController {
 	
-	@GetMapping("/")
-	public String home() {
-		return "index";
+	private final PostRepository postRepository;
+	private final HttpSession session;
+
+	public PostController(PostRepository postRepository, HttpSession session) {
+		this.postRepository = postRepository;
+		this.session = session;
+	}
+
+	@GetMapping({"/","/post"})
+	public String list(Model model) {
+		
+		model.addAttribute("postsEntity", postRepository.findAll());
+		return "post/list";
 	}
 }
