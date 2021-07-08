847ddfc7322c9d4f8a44ec68743e8e7483cffd75
DESKTOP-NUOPUIF\ITPS <heansum1556@gmail.com>
2021-07-08 17:55:28 +0900

    7. 게시글 삭제 구현(07/08)

--- src/main/java/com/korea/dbapp/web/PostController.java
+++ src/main/java/com/korea/dbapp/web/PostController.java
@@ -5,8 +5,14 @@ import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.korea.dbapp.domain.post.Post;
 import com.korea.dbapp.domain.post.PostRepository;
+import com.korea.dbapp.domain.user.User;
+import com.korea.dbapp.util.Script;
 
 @Controller
 public class PostController {
@@ -20,9 +26,48 @@ public class PostController {
 	}
 
 	@GetMapping({"/","/post"})
-	public String list(Model model) {
+	public String list(Model model) { // model = request
 		
 		model.addAttribute("postsEntity", postRepository.findAll());
-		return "post/list";
+		return "post/list"; // ViewResolver 도움!! + RequestDispatcher (request유지 기법)
+	}
+	
+	@GetMapping("/post/{id}")
+	public String detail(@PathVariable int id, Model model) {
+		Post postEntity = postRepository.findById(id).get();
+		model.addAttribute("postEntity", postEntity);
+		return "post/detail";
+	}
+	
+	@PostMapping("/post/{id}")
+	public @ResponseBody String deleteById(@PathVariable int id) {
+		// 1. 권한체크( post id를 통해 user id를 찾아서 session의 id를 비교) 
+		// 2. {id} 값으로 삭제
+		
+		// 세션의 user id 찾기 (session)
+		User principal = (User) session.getAttribute("principal");
+		int userId = principal.getId();
+		
+		// post의 user id 찾기 ({id})
+		Post postEntity = postRepository.findById(id).get(); // 이런건 어떻게 처리하지?
+		int postUserId = postEntity.getUser().getId();
+		
+		// postid를 넘겨 받아서 repository에 있는 id를 잡아서 삭제해준다
+		
+		// 2. {id} 값으로 삭제!!
+		if(userId==postUserId) {
+			postRepository.deleteById(id);
+			return Script.href("/");
+		} else {
+			return Script.back("삭제 실패");
+		}
+		
+	} // end of deleteById
+	
+	@GetMapping("/post/saveForm")
+	public String saveForm() {
+		// 1. 인증 체크 - 숙제
+		
+		return "post/saveForm";
 	}
 }
